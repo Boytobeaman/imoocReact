97ae953664591e4303a3fa49696504e29a708972
Anthony LU <luxingchaode@gmail.com>
2017-11-06 09:03:04 +0800

    first commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express();
+
+
+app.get('/', function (req, res) {
+	res.send('<h1>kkfak</h1>')
+})
+app.get('/data', function (req, res) {
+	res.json({naem: 'imooc', type: 'it react'})
+})
+
+app.listen(9999, function () {
+	console.log("listen on port 9999")
+})
