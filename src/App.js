790c445677e7f057070b8e8559adc56bd844aab0
Anthony Lu <anthony.x.lu@pwc.com>
2017-11-07 13:38:19 +0800

    add css tyle

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {createStore} from 'redux';
+
+import {Button, List} from 'antd-mobile';
+// import 'antd-mobile/dist/antd-mobile.css'
+
+// import {createStore} from 'redux';
 
 class App extends Component {
   render() {
+    const boss= 'liyunlong'
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>dulituan， leader{boss}</h2>
+        <FirstCamp boss='fistBoss'></FirstCamp>
+        <Cavalry boss='Sun Desheng'></Cavalry>
       </div>
     );
+  }
+}
+function Cavalry(props) {
+  return <h2>cavalry leader {props.boss} charge forward</h2>
+}
+class FirstCamp extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      soldiers: ['soldier one','soldier two', 'soldier three']
+    }
+    this.addSoldier = this.addSoldier.bind(this)
+  }
+  addSoldier(){
+    this.setState({
+      soldiers: [...this.state.soldiers, 'new soldier'+Math.random()]
+    })
+  }
+  render(){
+    return (
+      <div>
+        <h2>firstCamp leader {this.props.boss}</h2>
+        <Button type='primary' onClick={this.addSoldier}>add soldier</Button>
+        <List
+          renderHeader={()=>"soldier list"}
+        >
+          {this.state.soldiers.map(v=>{
+            return (
+              <List.Item key={v}>
+              {v}
+              </List.Item>
+            )
+          })}
+        </List>
+      </div>
+    )
   }
 }
 
