361f31414c62f7f7f56278380101c6360008ced3
Anthony Lu <anthony.x.lu@pwc.com>
2017-11-07 15:49:18 +0800

    add react-redux and redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,57 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import {Button, List} from 'antd-mobile';
-// import 'antd-mobile/dist/antd-mobile.css'
 
-// import {createStore} from 'redux';
-
-class App extends Component {
-  render() {
-    const boss= 'liyunlong'
-    return (
-      <div>
-        <h2>dulituan， leader{boss}</h2>
-        <FirstCamp boss='fistBoss'></FirstCamp>
-        <Cavalry boss='Sun Desheng'></Cavalry>
-      </div>
-    );
-  }
-}
-function Cavalry(props) {
-  return <h2>cavalry leader {props.boss} charge forward</h2>
-}
-class FirstCamp extends React.Component{
-  constructor(props) {
-    super(props)
-    this.state = {
-      soldiers: ['soldier one','soldier two', 'soldier three']
-    }
-    this.addSoldier = this.addSoldier.bind(this)
-  }
-  addSoldier(){
-    this.setState({
-      soldiers: [...this.state.soldiers, 'new soldier'+Math.random()]
-    })
-  }
-  render(){
-    return (
-      <div>
-        <h2>firstCamp leader {this.props.boss}</h2>
-        <Button type='primary' onClick={this.addSoldier}>add soldier</Button>
-        <List
-          renderHeader={()=>"soldier list"}
-        >
-          {this.state.soldiers.map(v=>{
-            return (
-              <List.Item key={v}>
-              {v}
-              </List.Item>
-            )
-          })}
-        </List>
-      </div>
-    )
-  }
+class App extends React.Component{
+	render(){
+		const store = this.props.store;
+		const num = store.getState();
+		const addGun = this.props.addGun;
+		const removeGun = this.props.removeGun;
+		const addGunAsync = this.props.addGunAsync;
+		return (
+			<div>
+				<h1>now we have {num} gun(s)</h1>
+				<button onClick={()=>store.dispatch(addGun())}>apply for arm</button>
+				<button onClick={()=>store.dispatch(removeGun())}>remove arm</button>
+				<button onClick={()=>store.dispatch(addGunAsync())}>delay arm</button>
+			</div>
+		) 
+	}
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDom from 'react-dom';
+import { createStore, applyMiddleware, compose} from 'redux';	
+import thunk from 'redux-thunk';	
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './app';
+import { counter, addGun ,removeGun, addGunAsync} from './index.redux';
+
+const store = createStore(counter, compose(
+		applyMiddleware(thunk),
+		window.devToolsExtension?window.devToolsExtension(): f=>f
+	));
+
+function render() {
+	ReactDom.render(<App store={store} addGunAsync={addGunAsync} addGun={addGun} removeGun={removeGun} />, document.getElementById("root"))
+}
+render();
+
+store.subscribe(render);
