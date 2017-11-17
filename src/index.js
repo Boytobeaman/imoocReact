6c4f1d1b53e22dd1ae84ae1c5178190cc0e28656
Anthony LU <luxingchaode@gmail.com>
2017-11-17 17:37:11 +0800

    react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 import { createStore, applyMiddleware, compose} from 'redux';	
 import thunk from 'redux-thunk';	
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Link} from 'react-router-dom';
 import App from './App';
 import { counter } from './index.redux';
 
@@ -10,13 +11,26 @@ const store = createStore(counter, compose(
 		applyMiddleware(thunk),
 		window.devToolsExtension ? window.devToolsExtension(): f=>f
 	));
-
-function render() {
-	ReactDom.render(
-		(<Provider store={store}>
-			<App />
-		</Provider>),
-		document.getElementById("root")
-	)
+function Erying() {
+	return <h2>二营</h2>
 }
-render();
+function Qibinglian() {
+    return <h2>骑兵连</h2>
+}
+ReactDom.render(
+    (<Provider store={store}>
+		<BrowserRouter>
+			<div>
+				<ul>
+					<li><Link to="/">一营</Link></li>
+					<li><Link to="/erying">二营</Link></li>
+					<li><Link to="/qibinglian">骑兵连</Link></li>
+				</ul>
+				<Route path="/" exact component={App}></Route>
+				<Route path="/erying" component={Erying}></Route>
+				<Route path="/qibinglian" component={Qibinglian}></Route>
+			</div>
+		</BrowserRouter>
+	</Provider>),
+    document.getElementById("root")
+);
