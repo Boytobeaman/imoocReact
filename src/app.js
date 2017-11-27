2bfbdd5f99db0bfc8abd8c3f95c1942f005f3b3a
Anthony Lu <anthony.x.lu@pwc.com>
2017-11-27 10:34:14 +0800

    add job.png and ignore .idea

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { addGun, removeGun, addGunAsync } from './index.redux';
+
+
+// App = connect(mapStatetoProps, actionCreators)(App);
+@connect(
+	state =>({num: state.counter}),
+    { addGun, removeGun, addGunAsync }
+    )
+class App extends React.Component{
+	render(){
+		const num = this.props.num;
+
+		return (
+			<div>
+				<h1>now we have {num} gun(s)</h1>
+				<button onClick={this.props.addGun}>apply for arm</button>
+				<button onClick={this.props.removeGun}>remove arm</button>
+				<button onClick={this.props.addGunAsync}>delay arm</button>
+			</div>
+		)
+	}
+}
+
+
+export default App
